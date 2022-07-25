f3a7bfc08ab38d1ebc783118a7bd66259e7e2101
Alpri Else <alprielse@gmail.com>
2022-07-24 20:34:32 -0400

    Implement list-empty-files command

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+use obby::{collect_files, get_file_contents};
+
 pub fn convert_dates() {
   //  TODO: Needs implementation
   println!("Converting dates!");
@@ -8,12 +11,28 @@ pub fn list_dangling_links() {
   println!("Listing dangling links!");
 }
 
-pub fn list_empty_files() {
-  //  TODO: Needs implementation
-  println!("Listing empty files");
+pub fn list_empty_files(working_directory: String) {
+  
+
+  let paths = collect_files(Path::new(&working_directory));
+
+  let mut empty_file_paths = Vec::new();
+  for path in paths {
+    if get_file_contents(Path::new(&path)).len() == 0 {
+      empty_file_paths.push(path);
+    }
+  }
+
+  pp_path_vec(&empty_file_paths);
 }
 
 pub fn list_referenceless_files() {
   //  TODO: Needs implementation
   println!("Listing referenceless files");
+}
+
+fn pp_path_vec(paths: &Vec<String>) {
+  for path in paths {
+    println!("{}", path);
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use std::env;
 
 mod commands;
 
@@ -6,34 +7,47 @@ mod commands;
 #[derive(Parser)]
 #[clap(name = "Obby")]
 struct Cli {
-    /// Name of the program to run
-    #[clap(subcommand)]
-    command: Commands
+  /// Name of the program to run
+  #[clap(subcommand)]
+  command: Commands
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    ConvertDates,
-    ListDanglingLinks,
-    ListEmptyFiles,
-    ListReferencelessFiles
+  ConvertDates,
+  ListDanglingLinks,
+  ListEmptyFiles {
+      #[clap(short, long, value_parser, default_value = "")]
+      directory: String,
+  },
+  ListReferencelessFiles
 }
 
 fn main() {
-    let args = Cli::parse();
+  let args = Cli::parse();
 
-    match args.command {
-        Commands::ConvertDates => {
-            commands::convert_dates();
-        }
-        Commands::ListDanglingLinks => {
-            commands::list_dangling_links();
-        }
-        Commands::ListEmptyFiles => {
-            commands::list_empty_files();
-        }
-        Commands::ListReferencelessFiles => {
-            commands::list_referenceless_files();
-        }
+  match args.command {
+    Commands::ConvertDates => {
+      commands::convert_dates();
     }
+    Commands::ListDanglingLinks => {
+      commands::list_dangling_links();
+    }
+    Commands::ListEmptyFiles { directory } => {
+      let cwd = match env::current_dir() {
+          Ok(path) => String::from(path.to_str().unwrap()),
+          Err(e) => {
+            println!("{}", e);
+            return
+          }
+        };
+
+      commands::list_empty_files(
+          if directory == "" { cwd } else { directory }
+      );
+    }
+    Commands::ListReferencelessFiles => {
+      commands::list_referenceless_files();
+    }
+  }
 }
